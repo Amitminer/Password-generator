bc310be3665f87b7eab7bd72053116acfcd52bed
AmitxD <81667979+Amitminer@users.noreply.github.com>
2023-05-04 20:09:18 +0530

    update passgen.kt

--- passgen.kt
+++ passgen.kt
@@ -1,1 +1,17 @@
+# importing random...
+import kotlin.random.Random
 
+# def function 
+fun main() {
+    val password = generatePassword()
+    println("Generated Password: $password")
+}
+
+fun generatePassword(length: Int): String {
+    val chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+"
+    val password = StringBuilder(length)
+    repeat(length) {
+        password.append(chars[Random.nextInt(chars.length)])
+    }
+    return password.toString()
+}
